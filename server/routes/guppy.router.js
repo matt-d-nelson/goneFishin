da1fc542fe338ef74ca80b72596b08cc4f8a19fa
Valentine Riedman <underblueskysismile@gmail.com>
2022-08-09 21:34:36 -0500

    added guppy GET in guppy router

---
+++ server/routes/guppy.router.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const pool = require("../modules/pool");
+const router = express.Router();
+
+/**
+ * GET route guppy
+ */
+router.get("/:id", (req, res) => {
+  // Get all cart items from a specific user where ordered = false
+  const queryString = `SELECT * FROM cart_items WHERE ordered = false AND WHERE cart_items.user_id = $1`;
+  const value = [req.user.id];
+  pool
+    .query(queryString, value)
+    .then((results) => {
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      console.log("Error in guppy GET router", err);
+      res.sendStatus(500);
+    });
+});
+
+/**
+ * POST route guppy
+ */
+router.post("/", (req, res) => {
+  // POST route code here
+});
+
+module.exports = router;
