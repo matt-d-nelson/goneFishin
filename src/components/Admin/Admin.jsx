3a3180e2b944d7d691dda91124cfb821f04783cf
Liz Horn <liz.mpls@gmail.com>
2022-08-11 16:24:04 -0500

    Fetch unfulfilled saga

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,8 +1,17 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {useHistory} from 'react-router-dom';
 
 function Admin( props ){
     // template hook
     const [ hook, setHook ] = useState( null );
+
+    const history = useHistory();
+    const dispatch = useDispatch();
+
+    useEffect(()=>{
+        dispatch({type: 'FETCH_UNFULFILLED'});
+    }, []);
      
     return(
         <div>
