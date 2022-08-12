501e12241e8244c3f72975ed41e410b228100952
Matt Nelson <mattnelson6992@gmail.com>
2022-08-12 12:32:57 -0500

    basic grid

--- src/components/Design/Design.jsx
+++ src/components/Design/Design.jsx
@@ -1,9 +1,9 @@
-import { Button, Grid, TextField } from "@material-ui/core";
+import { Button, Grid, TextField, Typography } from "@material-ui/core";
 import { useState } from "react";
 import chroma from "chroma-js";
 import LureSVG from "../LureSVG/LureSVG";
 
-function Design(props) {
+function Design() {
   const [bodyColor, setBodyColor] = useState("#00FF00");
   const [bodyShadeColor, setBodyShadeColor] = useState(
     chroma("#00FF00").darken()
@@ -30,77 +30,78 @@ function Design(props) {
   };
 
   return (
-    <div>
+    <div style={{ textAlign: "center" }}>
       <h2>Design</h2>
-      <p>Props: {JSON.stringify(props)}</p>
-      <Grid container spacing={2}>
+      <Grid container spacing={2} justifyContent="center">
         {/* //------------TEXT INPUTS------------// */}
-        <Grid item>
+        <Grid item xs={4}>
           <Grid container direction="column">
+            <Grid item>
+              <LureSVG
+                bodyColor={bodyColor}
+                bodyShadeColor={bodyShadeColor}
+                finColor={finColor}
+                dorsalColor={dorsalColor}
+                eyeColor={eyeColor}
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={2}>
+          <Grid container direction="column" spacing={2}>
             <Grid item>
               <TextField fullWidth label="title" />
             </Grid>
             <Grid item>
               <TextField fullWidth label="description" minRows={8} multiline />
             </Grid>
-          </Grid>
-        </Grid>
-        {/* //------------COLOR INPUTS------------// */}
-        <Grid item>
-          <Grid container direction="column">
-            <Grid item>
-              <Button component="label">
+            {/* //------------COLOR INPUTS------------// */}
+            <Grid item align="left">
+              <label>
                 <input
                   type="color"
                   onChange={handleBodyColorChange}
                   value={bodyColor}
+                  name="body"
                 />
-                Body
-              </Button>
+                : Body
+              </label>
             </Grid>
-            <Grid item>
-              <Button component="label">
+            <Grid item align="left">
+              <label>
                 <input
                   type="color"
                   onChange={handleFinColorChange}
                   value={finColor}
                 />
-                Fins
-              </Button>
+                : Fins
+              </label>
             </Grid>
-            <Grid item>
-              <Button component="label">
+            <Grid item align="left">
+              <label>
                 <input
                   type="color"
                   onChange={handleDorsalColorChange}
                   value={dorsalColor}
                 />
-                Dorsal
-              </Button>
+                : Dorsal
+              </label>
             </Grid>
-            <Grid item>
-              <Button component="label">
+            <Grid item align="left">
+              <label>
                 <input
                   type="color"
                   onChange={handleEyeColorChange}
                   value={eyeColor}
                 />
-                Eyes
-              </Button>
+                : Eyes
+              </label>
             </Grid>
           </Grid>
         </Grid>
         {/* //------------LURE SVG FLAT------------// */}
         <Grid item>
-          <Grid container direction="column">
-            <LureSVG
-              bodyColor={bodyColor}
-              bodyShadeColor={bodyShadeColor}
-              finColor={finColor}
-              dorsalColor={dorsalColor}
-              eyeColor={eyeColor}
-            />
-          </Grid>
+          <Grid container direction="column"></Grid>
         </Grid>
       </Grid>
     </div>
