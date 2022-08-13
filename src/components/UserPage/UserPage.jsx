048a861d41dd9ad1821d8046c520119fb67f531d
connorpeck <90285369+connorpeck@users.noreply.github.com>
2022-08-13 14:50:48 -0500

    edit and new buttons route to right places

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,19 +1,24 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
+import { useHistory } from 'react-router-dom';
 import "./UserPage.css";
 import {
   Button,
   Card,
   CardActions,
+  CardHeader,
+  IconButton,
   CardContent,
   CardMedia,
 } from "@mui/material";
 // import {Button} from '@mui/material'
-import DeleteIcon from '@mui/icons-material/Delete';
-import EditIcon from '@mui/icons-material/Edit';
-import DownloadIcon from '@mui/icons-material/Download';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import DownloadIcon from "@mui/icons-material/Download";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 
 function UserPage() {
   const dispatch = useDispatch();
@@ -21,6 +26,7 @@ function UserPage() {
   const user = useSelector((store) => store.user);
   const designs = useSelector((store) => store.home);
   const [current, setCurrent] = useState(0);
+  const history = useHistory();
 
   const length = designs.length;
 
@@ -32,8 +38,16 @@ function UserPage() {
 
   const addDesignToCart = () => {
     console.log("in addDesignToCart", designs[current].id);
-    dispatch({type: "ADD_DESIGN_TO_CART", payload: designs[current].id})
+    // const today = new Date().toLocaleDateString();
+    // console.log(today);
+    dispatch({ type: "ADD_DESIGN_TO_CART", payload: designs[current].id });
+   
   };
+
+  const newDesign = () => {
+    console.log('in newDesign');
+    history.push('/design')
+  }
 
   const downloadDesign = () => {
     console.log("in download design");
@@ -41,11 +55,16 @@ function UserPage() {
 
   const editDesign = () => {
     console.log("in editDesign");
+    history.push('/edit/:id')
   };
 
   const deleteDesign = () => {
-    console.log("in deleteDesign", designs[current].id, 'userID', user.id);
-    dispatch({type: "DELETE_DESIGN", payload: designs[current].id, id: designs[current].user_id})
+    console.log("in deleteDesign", designs[current].id, "userID", user.id);
+    dispatch({
+      type: "DELETE_DESIGN",
+      payload: designs[current].id,
+      id: designs[current].user_id,
+    });
     dispatch({ type: "FETCH_USER_DESIGNS" });
   };
 
@@ -65,7 +84,7 @@ function UserPage() {
     display: "grid",
     width: "26vw",
     transitionDuration: "0.3s",
-    height: "30vw",
+    height: "26vw",
     background: "#637a6b",
     color: "white",
   };
@@ -75,64 +94,102 @@ function UserPage() {
         RED WING LURE
         <br />
         COMPANY
-        <div className="imageSlider">
-          <p>{user.username}'s Designs</p>
-          <Button className="left-button" onClick={prevSlide}>
-                  Previous
-                </Button>
-                <Button className="right-button" onClick={nextSlide}>
-                  Next
-                </Button>
-          <div className="slider">
-
-       
-
-          {designs.map((design, index) => {
-            return (
-              <div key={index}>
-                
-                {index === current && ( 
+        <p>{user.username}'s Designs</p>
+        {designs.map((design, index) => {
+          return (
+            <div className="designs" key={index}>
+              {index === current && (
                 <div className="container">
-                
-                  <Card variant="outlined" style={cardStyle}>
-                    <CardMedia>
+                  <IconButton>
+                    <ChevronLeftIcon
+                      sx={{ fontSize: "80px" }}
+                      className="left-button"
+                      onClick={prevSlide}
+                    >
+                      Previous
+                    </ChevronLeftIcon>
+                  </IconButton>
+                  <IconButton>
+                    <ChevronRightIcon
+                      sx={{ fontSize: "80px" }}
+                      className="right-button"
+                      onClick={nextSlide}
+                    >
+                      Next
+                    </ChevronRightIcon>
+                  </IconButton>
+                  <Card elevation={4} style={cardStyle}>
+                    <CardHeader
+                      title={design.title}
+                      subheader={design.description}
+                    />
+
+                    <CardMedia
+                      component="img"
+                      height="300"
+                      image={design.image}
+                    />
+
+                    {/* <CardMedia>
                       <br />
                       <img src={design.image} alt="" />
                       <br /> Title: {design.title}
                       <p>id:{design.id}</p>
                       <p>svg colors: {design.svg_colors}</p>
                     </CardMedia>
-                    <CardContent>Description: {design.description}</CardContent>
+                    <CardContent>Description: {design.description}</CardContent> */}
+
                     <div className="cardButtons">
-                      <CardActions>
-                        <ShoppingCartIcon size="small" onClick={addDesignToCart}>
-                          Add To Cart
-                        </ShoppingCartIcon>
-                        <DownloadIcon size="small" onClick={downloadDesign}>
-                          Download
-                        </DownloadIcon>
-                        <EditIcon size="small" onClick={editDesign}>
-                          Edit
-                        </EditIcon>
-                        < DeleteIcon size="small" onClick={deleteDesign}>
-                          Delete
-                        </ DeleteIcon>
-                      </CardActions>
+                      <div className="centerButton">
+                        <CardActions>
+                          <IconButton>
+                            <ShoppingCartIcon
+                              size="small"
+                              onClick={addDesignToCart}
+                            >
+                              Add To Cart
+                            </ShoppingCartIcon>
+                          </IconButton>
+                          <IconButton>
+                            <DownloadIcon size="small" onClick={downloadDesign}>
+                              Download
+                            </DownloadIcon>
+                          </IconButton>
+                          <IconButton>
+                            <EditIcon size="small" onClick={editDesign}>
+                              Edit
+                            </EditIcon>
+                          </IconButton>
+                          <IconButton>
+                            <DeleteIcon size="small" onClick={deleteDesign}>
+                              Delete
+                            </DeleteIcon>
+                          </IconButton>
+                        </CardActions>
+                      </div>
                     </div>
                   </Card>
-                  
                 </div>
-                )}
-                
-              </div>
-            );
-          })}
-        </div>
-        </div>
-        <div className="buttonPlacement">
-          <Button>FEED</Button>
-          <Button>NEW</Button>
-        </div>
+              )}
+            </div>
+          );
+        })}
+      </div>
+      <div className="buttonPlacement">
+        <Button
+          className="feedButton"
+          variant="contained"
+          sx={{ fontSize: "25px", background: "#EDD892" }}
+        >
+          FEED
+        </Button>
+        <Button
+          variant="contained"
+          sx={{ fontSize: "25px", background: "#EDD892" }}
+          onClick={newDesign}
+        >
+          NEW
+        </Button>
       </div>
     </div>
   );
