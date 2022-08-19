71a88ed1e61d9462780e7c490f92f1ac5c1a9980
Liz Horn <liz.mpls@gmail.com>
2022-08-19 13:18:59 -0500

    path for /home goes to loginpage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -141,7 +141,7 @@ function App() {
               <Redirect to="/user" />
             ) : (
               // Otherwise, show the Landing page
-              <LandingPage />
+              <LoginPage />
             )}
           </Route>
 
