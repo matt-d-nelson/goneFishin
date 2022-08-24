0aae593c06368e858fb0dc80664543a48cb00557
Liz Horn <liz.mpls@gmail.com>
2022-08-23 19:48:27 -0500

    select and btn update quantity in cart

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import { Typography, Grid, Card, Button, Select, MenuItem } from "@mui/material";
 
@@ -11,6 +11,34 @@ function CartItem( {item} ){
     // on click, removes an item from cart
     const removeItem=(cartID)=>{
         dispatch({type: "DELETE_ORDER", payload: cartID});
+    }
+
+    // listen for change in qty locally and send dispatch to update in database when it changes
+    // useEffect(()=>{
+    //     console.log('in useEffect, qty: ', qty);
+    //     dispatch({type: 'UPDATE_CART_QTY',
+    //         payload: {
+    //             id: item.id,
+    //             qty: qty
+    //     }});
+    // }, [qty]);
+
+    // on change, update cart item qty in local state
+    const handleChange = (event) =>{
+        setQty(event.target.value);
+    }
+
+    // on click, update cart item quantity in database
+    const updateCartQty = () =>{
+        console.log('in updateCartQty, qty:', qty);
+        dispatch({
+            type: 'UPDATE_CART_QTY',
+            payload: {
+                id: item.id,
+                qty: qty,
+                message: 'Quantity updated'
+            }
+        });
     }
      
     return(
@@ -28,7 +56,7 @@ function CartItem( {item} ){
                     labelId='update-quantity-label'
                     value={qty}
                     label='New Quantity'
-                    // onChange={updateCart}
+                    onChange={handleChange}
                     >
                         <MenuItem value={1}>1</MenuItem>
                         <MenuItem value={2}>2</MenuItem>
@@ -41,6 +69,7 @@ function CartItem( {item} ){
                         <MenuItem value={9}>9</MenuItem>
                         <MenuItem value={10}>10</MenuItem>
                 </Select>
+                <Button onClick={updateCartQty}>Update Qty</Button>
             </Grid>
             <Grid item xs={2} m={2}>
                 <Button onClick={()=>{removeItem(item.id)}}>Remove</Button>
