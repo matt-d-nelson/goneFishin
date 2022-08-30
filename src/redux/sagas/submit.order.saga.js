e3041f60bc17e9fa725af2e68525c27404749b15
Matt Nelson <mattnelson6992@gmail.com>
2022-08-30 13:25:34 -0500

    added close modal dispatch to submit.order.saga.js

--- src/redux/sagas/submit.order.saga.js
+++ src/redux/sagas/submit.order.saga.js
@@ -1,27 +1,29 @@
-import { put, takeEvery } from 'redux-saga/effects';
-import axios from 'axios';
-
+import { put, takeEvery } from "redux-saga/effects";
+import axios from "axios";
 
 function* submitOrderSaga() {
-  yield takeEvery('ORDER_CART_ITEMS', submitOrder);
+  yield takeEvery("ORDER_CART_ITEMS", submitOrder);
 }
 
-function* submitOrder(){
+function* submitOrder() {
   // server request to mark cart item as ordered
-  try{
+  try {
     yield axios.put(`/api/guppy`);
     // saga to get all cart items
-    yield put({type: 'FETCH_CART_ITEMS'});
+    yield put({ type: "FETCH_CART_ITEMS" });
     // open modal to confirm successful ordering
     yield put({
       type: "OPEN_MODAL",
       payload: {
-        type: "success", 
-        open: true, 
-        success: 'Order submitted!'}
-    })
-  } catch(err){
-    console.log('submit order saga error:', err);
+        type: "success",
+        open: true,
+        success: "Order submitted!",
+      },
+    });
+    yield delay(1100);
+    yield put({ type: "CLOSE_MODAL" });
+  } catch (err) {
+    console.log("submit order saga error:", err);
   }
 }
 
