f0c3a1d9669a9bb141d2ab93ff4726ab93392747
connorpeck <90285369+connorpeck@users.noreply.github.com>
2022-09-04 12:38:09 -0500

    about page updated

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -27,7 +27,11 @@ const startDesign = () => {
     <div className="container">
        <Card sx={{ ml:50, mr:50 }}>
       <CardContent>
-   <CardHeader> Red Wing Lure Company </CardHeader>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Red Wing Lure Company 
+        </Typography>
+        </CardContent>
    The Chroma Lure Printer was born out of a desire to better understand the impact lure color has on catching fish. Whether helping fishermen in that pursuit, exploring their creativity, or creating personalized keepsakes, the Chroma Lure Printer and the web-based Lure Designer can make fishing more interesting, personal, and memorable without the need for expensive tools
 and complicated techniques. Anyone can create their own custom-designed fishing lures with
 ease and efficiency.
